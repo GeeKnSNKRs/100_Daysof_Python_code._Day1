85ec12a6f418c9021ce73b4ef6c8a6396bffb351
Chris Garcia <92829582+GeeKnSNKRs@users.noreply.github.com>
2022-02-16 10:20:23 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-print("Welcome to the band name generator.")                        #Welcoming Message
+print("Welcome to the band name generator.")                        #Launch Welcome Message for Program
 city = input("What's the name of the city you grew up in?\n")       #Get first value
 pet = input("What is the name of your pet?\n")                      #Get second value
 print("Your band name is..." + city + " " + pet)                    #Return Band name
